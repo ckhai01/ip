a1047b11da8b4546cb2846be80bd611b3a215afd
ckhai01 <ckhai0811@gmail.com>
2025-08-20 22:48:57 +0800

    Renamed 'Duke' to 'LeBot'. Added skeletal code for Level-0

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# LeBot User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/LeBot.java
@@ -0,0 +1,6 @@
+public class LeBot {
+    public static void main(String[] args) {
+        System.out.println("Yo, what’s good! It's LeBot James in the building! What can I help you with today? Let's get it!");
+        System.out.println("Ayy, take care! Hope to see you soon! Stay blessed.");
+    }
+}
