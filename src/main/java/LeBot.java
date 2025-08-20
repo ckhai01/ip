e2b2592e6246dd4544233ed45a1d0be98578f56d
ckhai01 <ckhai0811@gmail.com>
2025-08-20 23:12:58 +0800

    Added Echo function for Level 1.

--- src/main/java/LeBot.java
+++ src/main/java/LeBot.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class LeBot {
     public static void main(String[] args) {
         System.out.println("Yo, what’s good! It's LeBot James in the building! What can I help you with today? Let's get it!");
+        echo();
         System.out.println("Ayy, take care! Hope to see you soon! Stay blessed.");
+    }
+
+    public static void echo() {
+        String input;
+
+        while (true) {
+            Scanner inputScanner = new Scanner(System.in);
+            System.out.print("Enter: ");
+            input = inputScanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(input);
+        }
     }
 }
